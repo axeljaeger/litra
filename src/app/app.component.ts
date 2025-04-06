e00a29665feae650a9546bef947a6a40de2c9e11
Axel Jäger <axeljaeger@gmail.com>
2025-04-06 14:19:19 +0200

    Make application zoneless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 /// <reference types="w3c-web-hid" />
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { SingleLampControlComponent } from './single-lamp-control/single-lamp-control.component';
 
@@ -23,27 +23,25 @@ enum AppState {
     ]
 })
 export class AppComponent implements OnInit {
-  public appState : AppState = AppState.Idle;
+  public appState = signal<AppState>(AppState.Idle);
   public AppState = AppState;
 
   ngOnInit(): void {}
   public devices: HIDDevice[] | null = null;
 
   async start() {
-    this.appState = AppState.Connecting;
-
+    this.appState.set(AppState.Connecting);
     navigator.hid.addEventListener("disconnect", ({ device }) => {
       if (this.devices) {
         const deviceIndex = this.devices.indexOf(device);
         if (deviceIndex > -1) {
           this.devices?.splice(deviceIndex, 1);
           if (this.devices.length === 0) {
-            this.appState = AppState.Idle;
+            this.appState.set(AppState.Idle);
           }
         }
       }
     });
-
 
     this.devices = await navigator.hid.requestDevice({
       filters: [{
@@ -52,10 +50,10 @@ export class AppComponent implements OnInit {
       }]
     });
     if (this.devices.length > 0) {
-      this.appState = AppState.Connected;
+      this.appState.set(AppState.Connected);
       this.devices.forEach(async device => await device.open());
     } else {
-      this.appState = AppState.Idle;
+      this.appState.set(AppState.Idle);
     }
   }
 }
