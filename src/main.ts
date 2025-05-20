ab7587c4d168f7e6a2bc3e9442e43e91ee2ad1dd
Axel Jäger <axeljaeger@gmail.com>
2025-05-20 12:42:20 +0200

    Remove dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 import { isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
@@ -8,7 +7,6 @@ import { isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/
 bootstrapApplication(AppComponent, {
     providers: [
     provideExperimentalZonelessChangeDetection(),
-    provideAnimations(),
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
