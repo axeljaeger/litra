40795f71cf4d3e0a6224737a5e3a402255d9c9a7
Axel Jäger <axeljaeger@gmail.com>
2025-06-20 16:48:00 +0200

    Use vitest

--- src/app/welcome-page/welcome-page.component.spec.ts
+++ src/app/welcome-page/welcome-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomePageComponent } from './welcome-page.component';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('WelcomePageComponent', () => {
   let component: WelcomePageComponent;
@@ -8,7 +9,8 @@ describe('WelcomePageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [WelcomePageComponent]
+      imports: [WelcomePageComponent],
+      providers: [provideZonelessChangeDetection()]
     });
     fixture = TestBed.createComponent(WelcomePageComponent);
     component = fixture.componentInstance;
