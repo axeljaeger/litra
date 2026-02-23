a5e893042174fd7ff1b90a3eafbacf79ba606435
Axel Jäger <axeljaeger@gmail.com>
2026-02-23 18:33:01 +0100

    Fix storybook by adding skipLibCheck (#549)

--- src/app/single-lamp-control/single-lamp-control.component.stories.ts
+++ src/app/single-lamp-control/single-lamp-control.component.stories.ts
@@ -1,4 +1,4 @@
-import { type Meta, type StoryObj } from '@storybook/angular';
+import type { Meta, StoryObj } from '@storybook/angular';
 import { SingleLampControlComponent } from './single-lamp-control.component';
 
 const meta: Meta<SingleLampControlComponent> = {
